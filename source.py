87937575dfbc7ac3ca13a3a99b2805b043e5397c
CAN DURAN UNALDI <e2036523@ceng.metu.edu.tr>
2018-12-02 05:00:36 +0300

    first

--- source.py
+++ source.py
@@ -19,7 +19,7 @@ def generate_random_message():
 
 sock = socket(AF_INET, SOCK_STREAM)
 sock.connect(SOURCE_TO_BROKER.get_listener())
-while id<40:
+while id<1040:
     message = generate_random_message()
     print(message)
     sock.send(message.encode())  # TODO: Maybe send parts instead of whole message?
