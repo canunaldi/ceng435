7cec7540bef389af877a7c93bb9a8b7c318899ab
CAN DURAN UNALDI <e2036523@ceng.metu.edu.tr>
2018-12-02 04:18:35 +0300

    first

--- destination.py
+++ destination.py
@@ -12,7 +12,6 @@ result_datas = []
 def receive_r1():
     while True:
         data,addr = sock.recvfrom(18)
-        result_datas.append(data)
         currenttime = time.time()
         currenttime = currenttime * 1000
         currenttime = int (currenttime)
@@ -21,6 +20,8 @@ def receive_r1():
         starttime = str (starttime)
         starttime = int (starttime)
         end_to_end = currenttime - starttime
+        result_datas.append(end_to_end)
+
         print (end_to_end)
         ack = "OK!"
         sock.sendto(ack, DEST_TO_R1.get_sender())
@@ -33,7 +34,7 @@ def receive_r1():
 def receive_r2():
     while True:
         data,addr = sock2.recvfrom(18)
-        result_datas.append(data)
+        
         currenttime = time.time()
         currenttime = currenttime * 1000
         currenttime = int (currenttime)
@@ -42,6 +43,7 @@ def receive_r2():
         starttime = str (starttime)
         starttime = int (starttime)
         end_to_end = currenttime - starttime
+        result_datas.append(end_to_end)
         print (end_to_end)
         ack = "OK!"
         sock2.sendto(ack, DEST_TO_R2.get_sender())
